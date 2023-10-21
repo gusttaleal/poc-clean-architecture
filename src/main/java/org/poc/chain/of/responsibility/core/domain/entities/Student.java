454309560b77ff2056d986ab3778f5792c2ba799
gusttaleal <lses.gustavo@gmail.com>
2023-10-21 16:30:34 -0300

    fix: Fixing Student class

--- src/main/java/org/poc/chain/of/responsibility/core/domain/entities/Student.java
+++ src/main/java/org/poc/chain/of/responsibility/core/domain/entities/Student.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import org.poc.chain.of.responsibility.core.domain.valueobjects.CPF;
 import org.poc.chain.of.responsibility.core.domain.valueobjects.Email;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 @Data
 @Builder
@@ -13,17 +13,5 @@ public class Student {
     private final CPF cpf;
     private final String fullName;
     private final Email email;
-    private final Date birthDate;
-
-    public Student(
-            final CPF cpf,
-            final String fullName,
-            final Email email,
-            final Date birthDate
-    ) {
-        this.cpf = cpf;
-        this.fullName = fullName;
-        this.email = email;
-        this.birthDate = birthDate;
-    }
+    private final LocalDate birthDate;
 }
