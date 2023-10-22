7f47ce553ff08b1bdb5dba21c696f47f8dc8f8f7
gusttaleal <lses.gustavo@gmail.com>
2023-10-22 11:41:45 -0300

    feat: Developing persist student on database use case

---
+++ src/main/java/org/poc/chain/of/responsibility/core/domain/usecases/PersistStudentOnDatabase.java
@@ -0,0 +1,25 @@
+package org.poc.chain.of.responsibility.core.domain.usecases;
+
+import lombok.RequiredArgsConstructor;
+import org.poc.chain.of.responsibility.core.domain.entities.Student;
+import org.poc.chain.of.responsibility.core.domain.repositories.StudentRepository;
+
+import java.util.logging.Logger;
+
+@RequiredArgsConstructor
+public class PersistStudentOnDatabase {
+    private static final String className = PersistStudentOnDatabase.class.getSimpleName();
+
+    private static final Logger log = Logger.getLogger(className);
+
+    private final StudentRepository studentRepository;
+
+    public void execute(final Student student) {
+        log.info("[" + className + ":execute] Initiating the process to save Student data with enrollment code: ["
+                + student.getEnrollment() + "].");
+
+        studentRepository.persist(student);
+
+        log.info("[" + className + ":execute] The student data has been saved successfully!");
+    }
+}
