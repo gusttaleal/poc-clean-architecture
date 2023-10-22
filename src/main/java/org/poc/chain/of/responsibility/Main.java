3ccf54e0009d41b145b7cf2798b145f01a864959
gusttaleal <lses.gustavo@gmail.com>
2023-10-22 20:32:40 -0300

    feat: Test password flow

--- src/main/java/org/poc/chain/of/responsibility/Main.java
+++ src/main/java/org/poc/chain/of/responsibility/Main.java
@@ -1,12 +1,15 @@
 package org.poc.chain.of.responsibility;
 
 import org.poc.chain.of.responsibility.core.domain.entities.Student;
+import org.poc.chain.of.responsibility.core.domain.exceptions.ErrorWhenCipherPassword;
 import org.poc.chain.of.responsibility.core.domain.usecases.CreateStudentEnrollment;
+import org.poc.chain.of.responsibility.core.domain.usecases.CreateStudentPassword;
 import org.poc.chain.of.responsibility.core.domain.usecases.PersistStudentOnDatabase;
 import org.poc.chain.of.responsibility.core.domain.valueobjects.CPF;
 import org.poc.chain.of.responsibility.core.domain.valueobjects.Email;
 import org.poc.chain.of.responsibility.infra.database.StudentMongoRepository;
 import org.poc.chain.of.responsibility.infra.database.StudentPostgresRepository;
+import org.poc.chain.of.responsibility.infra.service.JavaxCryptoCipher;
 
 import java.time.LocalDate;
 import java.util.Objects;
@@ -18,8 +21,9 @@ public class Main {
     private static final CreateStudentEnrollment createStudentEnrollment = new CreateStudentEnrollment();
     private static final StudentPostgresRepository studentPostgresRepository = new StudentPostgresRepository();
     private static final StudentMongoRepository studentMongoRepository = new StudentMongoRepository();
+    private static final JavaxCryptoCipher javaxCryptoCipher = new JavaxCryptoCipher();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ErrorWhenCipherPassword {
         /*Simulation of an infrastructure component*/
         log.info("Application start!");
 
@@ -40,6 +44,10 @@ public class Main {
         log.info("Student data: [" + student + "].");
 
         createStudentEnrollment.execute(student);
+        log.info("Updated student data : [" + student + "].");
+
+        CreateStudentPassword createStudentPassword = new CreateStudentPassword(javaxCryptoCipher);
+        createStudentPassword.execute(student, "123asd");
         log.info("Updated student data : [" + student + "].");
 
         persistStudentOnDatabase.execute(student);
